1d4a66b5b4af384d5c3b22c1f937b7b69590567f
Thomas HAUTIER <thomas.hautier@informatique.univ-paris-diderot.fr>
2017-03-24 15:07:01 +0100

    Début gestion de carte

--- Pistolero/src/Jeu.java
+++ Pistolero/src/Jeu.java
@@ -1,12 +1,27 @@
+import Controller.Controller;
+import Model.*;
+import View.*;
 import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Jeu extends Application{
 
+	Model m;
+	View v;
+	Controller c;
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
 	@Override
-	public void start(Stage primaryStage) throws Exception {
-		System.out.println("Hello world");
-		
+	public void start(Stage primaryStage) {
+
+		Group root = new Group();
+		primaryStage.setScene(new Scene(root, 800, 600));
+		primaryStage.show();
 	}
 
 }
